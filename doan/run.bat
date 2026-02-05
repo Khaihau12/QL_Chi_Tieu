5a1491f6f320ba9a0ed0ae3b1f1dc9d04f7e9a5d
Khaihau12 <khaihau100@gmail.com>
2026-02-05 15:58:36 +0700

    Update run.bat

--- doan/run.bat
+++ doan/run.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d "%~dp0"
 echo Running JavaFX Application...
-java --module-path "%USERPROFILE%\.m2\repository\org\openjfx\javafx-controls\13;%USERPROFILE%\.m2\repository\org\openjfx\javafx-fxml\13;%USERPROFILE%\.m2\repository\org\openjfx\javafx-graphics\13;%USERPROFILE%\.m2\repository\org\openjfx\javafx-base\13;target\classes" --add-modules javafx.controls,javafx.fxml -cp "target\classes;%USERPROFILE%\.m2\repository\mysql\mysql-connector-java\8.0.33\mysql-connector-java-8.0.33.jar" com.example.App
+java --module-path "%USERPROFILE%\.m2\repository\org\openjfx\javafx-controls\13;%USERPROFILE%\.m2\repository\org\openjfx\javafx-graphics\13;%USERPROFILE%\.m2\repository\org\openjfx\javafx-base\13;target\classes" --add-modules javafx.controls -cp "target\classes;%USERPROFILE%\.m2\repository\mysql\mysql-connector-java\8.0.33\mysql-connector-java-8.0.33.jar" com.example.App
 pause
